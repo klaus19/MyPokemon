3763d1d219d6a675921c75d7af4c87d59c63471b
Tejas khartude <tejas.khartude@clariontechnologies.co.in>
2022-04-12 16:58:50 +0530

    Added new file

--- app/src/main/java/com/example/mypokemon/network/PokedexClient.kt
+++ app/src/main/java/com/example/mypokemon/network/PokedexClient.kt
@@ -1,8 +1,30 @@
 package com.example.mypokemon.network
 
+import com.example.mypokemon.model.PokemonInfo
+import com.example.mypokemon.model.PokemonResponse
+import com.skydoves.sandwich.ApiResponse
 import javax.inject.Inject
 
 class PokedexClient @Inject constructor(
+     private val pokedexService: PokedexService
+) {
 
-){
+    suspend fun fetchPokemonList(
+        page: Int
+    ): ApiResponse<PokemonResponse> =
+        pokedexService.fetchPokemonList(
+            limit = PAGING_SIZE,
+            offset = page * PAGING_SIZE
+        )
+
+    suspend fun fetchPokemonInfo(
+        name: String
+    ): ApiResponse<PokemonInfo> =
+        pokedexService.fetchPokemonInfo(
+            name = name
+        )
+
+    companion object {
+        private const val PAGING_SIZE = 20
+    }
 }
