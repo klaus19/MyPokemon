6f60136d797e86bdd8c4ca39f8e904724c6c8a27
tejaskhartude <tejaskhartude@gmail.com>
2022-05-22 11:50:42 +0530

    Updated the MainRepository

--- app/src/main/java/com/example/mypokemon/Repository/MainRepository.kt
+++ app/src/main/java/com/example/mypokemon/Repository/MainRepository.kt
@@ -1,12 +1,60 @@
 package com.example.mypokemon.Repository
 
+import androidx.annotation.WorkerThread
+import com.example.mypokemon.mapper.ErrorResponseMapper
+import com.example.mypokemon.model.Pokemon
 import com.example.mypokemon.network.PokedexClient
 import com.example.mypokemon.persistence.PokemonDao
+import com.skydoves.sandwich.map
+import com.skydoves.sandwich.onError
+import com.skydoves.sandwich.onException
+import com.skydoves.sandwich.suspendOnSuccess
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 
 class MainRepository(
-    pokedexClient: PokedexClient,
-    pokemonDao: PokemonDao,
-    coroutineDispatcher: CoroutineDispatcher
-) {
-}
+    private val pokedexClient: PokedexClient,
+    private val pokemonDao: PokemonDao,
+    private val ioDispatcher: CoroutineDispatcher
+) : Repository {
+
+    @WorkerThread
+    fun fetchPokemonList(
+        page: Int,
+        onStart: () -> Unit,
+        onComplete: () -> Unit,
+        onError: (String?) -> Unit
+    ) = flow {
+        var pokemons = pokemonDao.getPokemonList(page)
+        if (pokemons.isEmpty()) {
+            /**
+             * fetches a list of [Pokemon] from the network and getting [ApiResponse] asynchronously.
+             * @see [suspendOnSuccess](https://github.com/skydoves/sandwich#apiresponse-extensions-for-coroutines)
+             */
+            val response = pokedexClient.fetchPokemonList(page = page)
+            response.suspendOnSuccess {
+                pokemons = data.results
+                pokemons.forEach { pokemon -> pokemon.page = page }
+                pokemonDao.insertPokemonList(pokemons)
+                emit(pokemonDao.getAllPokemonList(page))
+            }
+                // handles the case when the API request gets an error response.
+                // e.g., internal server error.
+                .onError {
+                    /** maps the [ApiResponse.Failure.Error] to the [PokemonErrorResponse] using the mapper. */
+                    map(ErrorResponseMapper.ErrorResponseMapper) { onError("[Code: $code]: $message") }
+                }
+                // handles the case when the API request gets an exception response.
+                // e.g., network connection error.
+                .onException {
+                    onError(message)
+                }
+        }
+        else {
+            emit(pokemonDao.getAllPokemonList(page))
+        }
+    }.onStart { onStart() }.onCompletion { onComplete() }.flowOn(ioDispatcher)
+            }
